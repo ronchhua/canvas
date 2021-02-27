9c008244e7b171d6f52f7dbfd7bc38d715f0996c
ronchhua <ronchhua321@gmail.com>
2021-02-27 00:41:11 -0800

    Added some comments so that I can try and remember my thought process

--- src/store/store.js
+++ src/store/store.js
@@ -14,14 +14,16 @@ export const store = new Vuex.Store( {
         rectangle: {  
             x: 50,
             y: 50,
-            origX: 50,
+            origX: 50,              // Keep a state representing the original coordinates on the canvas. Used for animating rotation.
             origY: 50,
-            center: [100, 137.5],   // (x1 + (x2+width)) / 2
-            origWidth: 100,
-            origHeight: 175,
-            width: 100,
+            center: [100, 137.5],   // (x1 + (x1+width)) / 2  -  (y1 + (y1+height)) / 2
+
+            origWidth: 100,         // Keep a state representing the original width and height 
+            origHeight: 175,        // because height and width are modified to correctly allow for bound detection in App.vue and so that we have a state (coordinates) represented by the rotating object.
+            width: 100,             
             height: 175,
-            maintainedCur: [50, 50],
+            rotatedCoord: [50, 50],    // This is necessary to maintain the top-left corner as it rotates. We need to maintain it because we overwrite 'x' and 'y'
+                                        // so that when we call updateCoordinates(), we can get the proper Cartesian version of the coordinates and use it for calculations.
         },
         // do paint and do the rotate there
         circle: {
@@ -45,7 +47,7 @@ export const store = new Vuex.Store( {
             origHeight: 200,
             width: 250,
             height: 200,
-            maintainedCur: [250, 50],
+            rotatedCoord: [250, 50],
         },
         {
             imageName: 'Hearthstone', 
@@ -58,7 +60,7 @@ export const store = new Vuex.Store( {
             origHeight: 200,
             width: 250,
             height: 200,
-            maintainedCur: [50, 300],
+            rotatedCoord: [50, 300],
         }
         ],
     },
@@ -72,37 +74,37 @@ export const store = new Vuex.Store( {
 
         updateCoordinates (state) {
 
-            state.rectangle.x = state.rectangle.maintainedCur[0];
-            state.rectangle.y = state.rectangle.maintainedCur[1];
+            state.rectangle.x = state.rectangle.rotatedCoord[0];   // Set the x value to the previous calculated rotated x-coord
+            state.rectangle.y = state.rectangle.rotatedCoord[1];   // Set the y value to the previous calculated rotated y-coord
 
-            state.rectangle.x -= state.rectangle.center[0];
-            state.rectangle.y -= state.rectangle.center[1];
+            state.rectangle.x -= state.rectangle.center[0];         // Now take the canvas's coordinate plane and offset it to match the cartesian plane
+            state.rectangle.y -= state.rectangle.center[1];         // For both x and y coordinates.
 
-            state.images.forEach((image) => {
+            state.images.forEach((image) => {              // Do the same for the images like I did for rectangle.
 
-                image.x = image.maintainedCur[0];
-                image.y = image.maintainedCur[1];
+                image.x = image.rotatedCoord[0];
+                image.y = image.rotatedCoord[1];
 
                 image.x -= image.center[0];
                 image.y -= image.center[1];
             });
 
-            var finalCoords = [];
-            var elements = [];
+            var finalCoords = [];   // Store the computed rotated coordinate (the canvas' coordinate) after applying a rotation of 90 degrees.
+            var elements = [];      // Just used to hold the rectangle and images in an array, so that I don't have to keep separating their calculations.
           
-            var convertedRadians = 90 * (Math.PI / 180);   // Rotate 359->270
+            var convertedRadians = 90 * (Math.PI / 180);   // Convert the angle we are rotating by to radians.
 
 
-            var rotatedX = state.rectangle.x*Math.cos(convertedRadians) - state.rectangle.y*Math.sin(convertedRadians);
+            var rotatedX = state.rectangle.x*Math.cos(convertedRadians) - state.rectangle.y*Math.sin(convertedRadians);     // Apply the rotation
             var rotatedY = state.rectangle.x*Math.sin(convertedRadians) + state.rectangle.y*Math.cos(convertedRadians);
             
-            var positionedX = rotatedX+state.rectangle.center[0];
+            var positionedX = rotatedX+state.rectangle.center[0];   // Convert x and y back into canvas' coordinate values
             var positionedY = rotatedY+state.rectangle.center[1];
 
-            elements.push(state.rectangle);
-            finalCoords.push([positionedX, positionedY]);
+            elements.push(state.rectangle);                 // Add rectangle into our array, could have done it way earlier.
+            finalCoords.push([positionedX, positionedY]);   // Save the correct canvas' coordinates for the rotation on (x,y) 
 
-            state.images.forEach((image) => {
+            state.images.forEach((image) => {       // Do the same for all our images like we did for rectangle
 
                 var convertedRadians = 90 * (Math.PI / 180);   // Rotate 359->270
 
@@ -117,24 +119,31 @@ export const store = new Vuex.Store( {
             
             });
             
-            var holder;
-            var index = 0;
+            var holder;     // A temp variable used to hold the width value before overwriting and writing to the height
+            var index = 0;  // Iterator for the coordinates in finalCoords
+
+            /* The intuition behind each case is that, for rectangle and the images, we only maintain 1 point that represents their placement on the canvas
+            *  and that is always the top left point of each element. So when we do a rotation, we need to change (x,y) such that our "abstract" rectangle
+            *  (necessary for detecting mouse hovers) is properly interpreted by the inRect() and inImages() function so that we can detect the hovering on
+            *  the rotated elements as shown on the canvas. In those methods, we always assumed a top-left coordinate and check if the mouse is in that bound
+            *  of the top-left coordinate and the width and height.
+            */
 
             switch(state.rotation) {
-                case 90:
+                case 90:    // If current rotation is 90
                     elements.forEach((element) => {
-                        element.x = finalCoords[index][0] - element.origHeight;
-                        element.y = finalCoords[index][1];
+                        element.x = finalCoords[index][0] - element.origHeight;     // Modify the rectangle and images' (x,y) such that they are always appear as the top-left angle of the shape. 
+                        element.y = finalCoords[index][1];                          // This is necessary because we can only draw rectangles starting from a (x,y) that represents the top left. And we use width and height to change the size of the retangle.
                         
-                        holder = element.width;
-                        element.width = element.height;
+                        holder = element.width;                     // For a 90 degree rotation, we will be swapping the height and width so that
+                        element.width = element.height;             // when (x,y) is positioned correctly, and the rectangle is rotated to 90 degree, the height represents the sideway rectangle's width.
                         element.height = holder;
                         index++;
                     });
                     break;
-                case 180:
-                    elements.forEach((element) => {
-                        element.x = finalCoords[index][0] - element.origWidth;
+                case 180:   // If current rotation is 180
+                    elements.forEach((element) => {                                 
+                        element.x = finalCoords[index][0] - element.origWidth;      
                         element.y = finalCoords[index][1] - element.origHeight;
 
                         holder = element.width;             // We basically make the width equal back to the width, that was changed in 90 degree rotation.
@@ -144,7 +153,7 @@ export const store = new Vuex.Store( {
                         index++;
                     });
                     break;
-                case 270:
+                case 270:   // If current rotation is 270
                     elements.forEach((element) => {
                         element.x = finalCoords[index][0];
                         element.y = finalCoords[index][1] - element.origWidth;
@@ -156,7 +165,7 @@ export const store = new Vuex.Store( {
 
                     });
                     break;
-                case 360:
+                case 360:   // If current rotation is 360 (0)
                     elements.forEach((element) => {
                         element.x = finalCoords[index][0];
                         element.y = finalCoords[index][1];
@@ -172,14 +181,14 @@ export const store = new Vuex.Store( {
 
             index=0;
             elements.forEach((element) => {
-                element.maintainedCur[0] = finalCoords[index][0];
-                element.maintainedCur[1] = finalCoords[index][1];
+                element.rotatedCoord[0] = finalCoords[index][0];        // We store each element's rotated coordinate on the canvas
+                element.rotatedCoord[1] = finalCoords[index][1];
                 index++;
             });
 
 
-            console.log(finalCoords[0][0], finalCoords[0][1]);
-            console.log(state.rectangle.x, state.rectangle.y);
+            //console.log(finalCoords[0][0], finalCoords[0][1]);
+            //console.log(state.rectangle.x, state.rectangle.y);
 
 
         },
